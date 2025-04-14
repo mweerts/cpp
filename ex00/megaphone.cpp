65ee8ff1de69d7d7e8ddd8f5e072d021c794acda
mweerts <maxweert@student.s19.be>
2025-04-14 17:53:41 +0200

    modifs ex00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,21 +1,19 @@
 #include <iostream>
 #include <string>
 
-int main(int argc, char **argv) {
-	std::string tmp;
-	if (argc < 2)
-	{
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
-		return (0);
-	}
-	for (int i = 1; i < argc; i++)
-	{
-		tmp = argv[i];
-		for (int j = 0; argv[i][j]; j++)
-			std::cout << (char)std::toupper(argv[i][j]);
-		if (argv[i + 1])
-			std::cout << " ";
-	}
-	std::cout << "\n";
+int main(int argc, char** argv) {
+    std::string tmp;
+    if (argc < 2) {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+        return (0);
+    }
+    for (int i = 1; i < argc; i++) {
+        tmp = argv[i];
+        for (int j = 0; argv[i][j]; j++)
+            std::cout << (char)std::toupper(argv[i][j]);
+        if (argv[i + 1])
+            std::cout << " ";
+    }
+    std::cout << "\n";
     return (0);
 }
