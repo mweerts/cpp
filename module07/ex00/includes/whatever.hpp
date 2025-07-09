a1cf9c35867d1a3dba45c73bc282344655746427
maxweert <maxweert@student.s19.be>
2025-07-09 16:15:41 +0200

    ex00

---
+++ module07/ex00/includes/whatever.hpp
@@ -0,0 +1,25 @@
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+template <typename T>
+T swap(T& a, T& b) {
+    T tmp;
+
+    tmp = a;
+    a = b;
+    b = tmp;
+    return a;
+}
+
+template <typename T>
+T min(T a, T b) {
+    a = 10;
+    return (a < b ? a : b);
+}
+
+template <typename T>
+T max(T a, T b) {
+    return (a > b ? a : b);
+}
+
+#endif
