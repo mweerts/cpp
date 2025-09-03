3534685f0a1faf460227d13aeae23bea7f068706
maxweert <maxweert@student.s19.be>
2025-09-04 00:36:40 +0200

    ex01 should be ok

--- module07/ex01/iter.hpp
+++ module07/ex01/iter.hpp
@@ -1,5 +1,29 @@
 #ifndef ITER_HPP
 #define ITER_HPP
 
+template <typename T, typename Function>
+void iter(T* array, size_t n, Function func) {
+    for (size_t i = 0; i < n; i++) {
+        func(array[i]);
+    }
+}
+
+template <typename T>
+void iter(T* array, size_t n, void (*func)(T&)) {
+    if (!func)
+        return;
+    for (size_t i = 0; i < n; i++) {
+        func(array[i]);
+    }
+}
+
+template <typename T>
+void iter(T* array, size_t n, void (*func)(const T&)) {
+    if (!func)
+        return;
+    for (size_t i = 0; i < n; i++) {
+        func(array[i]);
+    }
+}
 
 #endif
--- module07/ex01/main.cpp
+++ module07/ex01/main.cpp
@@ -1,18 +1,38 @@
 #include <iostream>
+#include <string>
+
 #include "iter.hpp"
 
-int main(void) {
-    int a = 2;
-    int b = 3;
-    ::swap(a, b);
-    std::cout << "a = " << a << ", b = " << b << std::endl;
-    std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
-    std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
-    std::string c = "chaine1";
-    std::string d = "chaine2";
-    ::swap(c, d);
-    std::cout << "c = " << c << ", d = " << d << std::endl;
-    std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
-    std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
+void printIntValue(int x) {
+    std::cout << "int value: " << x << std::endl;
+}
+void printInt(int& x) {
+    std::cout << "int&: " << x << std::endl;
+}
+void printConstInt(const int& x) {
+    std::cout << "const int&: " << x << std::endl;
+}
+void printString(std::string& s) {
+    std::cout << "string&: " << s << std::endl;
+}
+void printConstString(const std::string& s) {
+    std::cout << "const string&: " << s << std::endl;
+}
+
+int main() {
+    int         arr[] = {1, 2, 3};
+    std::string strs[] = {"a", "b", "c"};
+
+    std::cout << "1. Avec fonction prenant T (valeur)\n";
+    iter(arr, 3, printIntValue);
+
+    std::cout << "\n2. Avec fonction prenant T& (référence)\n";
+    iter(arr, 3, printInt);
+    iter(strs, 3, printString);
+
+    std::cout << "\n3. Avec fonction prenant const T& (référence const)\n";
+    iter(arr, 3, printConstInt);
+    iter(strs, 3, printConstString);
+
     return 0;
 }
