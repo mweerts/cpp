497d7aa77e975fbe4c26545201c7e6d6dc5459bb
maxweert <maxweert@student.s19.be>
2025-11-20 03:50:36 +0100

    should be ok, have to test at school

--- module07/ex00/main.cpp
+++ module07/ex00/main.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
+
 #include "whatever.hpp"
 
 int main(void) {
     int a = 2;
     int b = 3;
+
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "Swaping items ..." << std::endl;
     ::swap(a, b);
     std::cout << "a = " << a << ", b = " << b << std::endl;
     std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
     std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+
+    std::cout << "\n------------------------\n" << std::endl;
+
     std::string c = "chaine1";
     std::string d = "chaine2";
+
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "Swaping items ..." << std::endl;
     ::swap(c, d);
     std::cout << "c = " << c << ", d = " << d << std::endl;
     std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
